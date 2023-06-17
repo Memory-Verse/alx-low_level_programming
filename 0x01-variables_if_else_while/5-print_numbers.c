a13798a1380fbb1aa2a889977925f67531f1bf43
Gbagome Livingstone <memoryverse0@gmail.com>
2023-06-17 05:02:29 +0000

    prints digits from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base ten digits starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
